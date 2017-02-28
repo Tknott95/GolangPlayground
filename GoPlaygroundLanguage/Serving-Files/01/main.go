313f5100be820139d40e7fbecf7806fa8b47f542
tknott95 <tknott95@hotmail.com>
2017-02-27 21:12:25 -0700

    Serving IMG - Header

--- GoPlaygroundLanguage/Serving-Files/01/main.go
+++ GoPlaygroundLanguage/Serving-Files/01/main.go
@@ -10,11 +10,12 @@ func main() {
     http.HandleFunc("/", pictureExample)
     http.ListenAndServe(":8080", nil)
 
-    func pictureExample(w http.ResponseWriter, req *http.Request) {
-        w.Header().Set("Content-Type", "text/html: charset=utf-8")
-
-        io.WriteString(w, 
-        <img src="http://debatepost.com/wp-content/uploads/2016/07/anonymous-1200.png">
-        )
-    }
 }
+
+ func pictureExample(w http.ResponseWriter, req *http.Request) {
+        w.Header().Set("Content-Type", "text/html; charset=utf-8")
+
+        io.WriteString(w, `
+                <img src="http://debatepost.com/wp-content/uploads/2016/07/anonymous-1200.png">
+                `)
+    }
